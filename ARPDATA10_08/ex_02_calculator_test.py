83eac1ab4d336433d047ca6506e875f3c10c6c4d
kleofas971 <mika.adam@outlook.com>
2023-03-08 18:26:40 +0100

    adding tests for students

---
+++ ARPDATA10_08/ex_02_calculator_test.py
@@ -0,0 +1,10 @@
+import unittest
+from ARPDATA10_07.ex_02_calculator import Calculator
+
+
+class CalculatorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = Calculator(5,2)
+    # dopisz testy dla kalkulatora :)
+
